f6b7749a5de8eb6b8ed258f64801323ba09c8201
connor b <31991894+connorjose@users.noreply.github.com>
2025-11-12 13:39:02 +1000

    Add note functionality working

--- src/components/AppSidebar.tsx
+++ src/components/AppSidebar.tsx
@@ -14,14 +14,20 @@ import { useNotes } from "@/context/NotesContext";
 export function AppSidebar()
 {
     // const { notes, selectedIndex, editNote } = useNotes();
-    const { notes, selectedIndex, setSelectedIndex } = useNotes();
+    const { notes, selectedIndex, setSelectedIndex, addNote } = useNotes();
     
     return (
         <Sidebar variant="sidebar">
             <SidebarContent>
                 <SidebarGroup>
                     <SidebarGroupLabel>Notes</SidebarGroupLabel>
-                    <SidebarGroupAction title="Add note">
+                    <SidebarGroupAction 
+                        title="Add note"
+                        onClick={(e) => {
+                            e.preventDefault();
+                            addNote();
+                        }}
+                    >
                         <Plus/><span className="sr-only">Add Note</span>
                     </SidebarGroupAction>
                     <SidebarGroupContent>
