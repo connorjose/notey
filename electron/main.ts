d48b8149e2b63b365e3e5fdeba67fc2b338c644b
connor b <31991894+connorjose@users.noreply.github.com>
2025-12-01 21:07:39 +1000

    Create WindowManager and apply to main

--- electron/main.ts
+++ electron/main.ts
@@ -1,23 +1,23 @@
 import { app, BrowserWindow } from 'electron'
 import { registerHandlers } from './handlers/NoteIPCHandlers'
-import { createWindow, closeWindow } from './handlers/Window';
+import { windowManager } from './handlers/Window';
 import noteService from './services/NoteService'
 
 app.whenReady().then(() => {
   registerHandlers();
-  createWindow();
+  windowManager.createWindow();
 }, (error) => console.error('Error loading app:', error));
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     noteService.close();
-    closeWindow();
+    windowManager.closeAll();
     app.quit()
   }
 })
 
 app.on('activate', () => {
   if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow()
+    windowManager.createWindow();
   }
 })
